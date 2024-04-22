8623a518aa7273eed39248eae73f1f1a31b649cb
EgbertLudema <EgbertLudema2001@gmail.com>
2024-04-22 10:59:39 +0200

    1st working version

---
+++ app/Filament/Resources/LocationsResource.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\LocationsResource\Pages;
+use App\Filament\Resources\LocationsResource\RelationManagers;
+use App\Models\Locations;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class LocationsResource extends Resource
+{
+    protected static ?string $model = Locations::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form->schema([
+            // Define your form fields here based on your locations table columns
+            Forms\Components\TextInput::make('name'),
+        ]);
+    }
+
+
+    public static function table(Table $table): Table
+    {
+        return $table->columns([
+            Tables\Columns\TextColumn::make('name'),
+        ])->filters([
+            //
+        ])
+        ->actions([
+            Tables\Actions\EditAction::make(),
+        ])
+        ->bulkActions([
+            Tables\Actions\BulkActionGroup::make([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]),
+        ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListLocations::route('/'),
+            'create' => Pages\CreateLocations::route('/create'),
+            'edit' => Pages\EditLocations::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getModel(): string
+    {
+        return Locations::class;
+    }
+}
---
+++ app/Filament/Resources/LocationsResource/Pages/ListLocations.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\LocationsResource\Pages;
+
+use App\Filament\Resources\LocationsResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListLocations extends ListRecords
+{
+    protected static string $resource = LocationsResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
